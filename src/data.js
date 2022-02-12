d955465f50f91c6873f1aa9e77954affd8918df0
Shashi <shashikant.py@gmail.com>
2022-02-12 22:51:59 +0530

    bdy

---
+++ src/data.js
@@ -0,0 +1,40 @@
+export default [
+    {
+      id: 1,
+      name: 'Shashi kant kumar',
+      age: 22,
+      image:
+        // 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+        'https://i.ibb.co/whp7JPB/img2.jpg',
+    },
+    {
+      id: 2,
+      name: 'Varinder Pr',
+      age: 23,
+      image:
+        // 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+        'https://i.ibb.co/3CZYtRZ/IMG-20211122-134310.jpg',
+    },
+    {
+      id: 3,
+      name: 'Supreme Personality of Godhead',
+      age: 2000,
+      image:
+        // 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+        'https://i.ibb.co/m6MqNLN/kannha.jpg',
+    },
+    {
+      id: 4,
+      name: 'Shashi kant kumar',
+      age: 22,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    },
+    {
+      id: 5,
+      name: 'Lola Gardner',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+  ];
